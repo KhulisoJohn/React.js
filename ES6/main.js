728e1ecd26aefa34f05a70623178374c3b7bf07d
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-03-17 11:01:57 +0200

    initial commit

---
+++ ES6/main.js
@@ -0,0 +1,19 @@
+//Logical AND
+
+let a = true;
+let b = false;
+
+function getName(name){
+ return name;
+}
+
+
+console.log(b && getName("khulyso"));
+
+
+// Logical OR
+
+const getLastName = (lastName) => {
+    return getLastName;
+}
+ console.log(a || getLastName("john"));
--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,11 @@
-
 import './App.css'
 
-function App() {
+const App = () => {
 
-
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
+  return(
+    <h1>functional arror function</h1>
   )
+
 }
 
 export default App
